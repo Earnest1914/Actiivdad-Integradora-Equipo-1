35d93e276e224e95af933b544b961ceea967d4a8
Earnest1914 <112690649+Earnest1914@users.noreply.github.com>
2022-09-03 02:29:29 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <p className="App">
+        <h3>1.0 - Proyecto </h3>Como dueño del sistema (empresa “Taller mecánico San Efra”) quiero que se me desarrolle un sitio web con el fin de ofrecer mis servicios en el mismo. Condiciones: El proyecto ha de cumplir con mis expectativas (como cliente) y habré de aprobarlo.
+        </p>
+        <p className="App">
+        <h3>1.1.1 - HTML 1 </h3>Como dueño del sistema quiero que la página principal aloje menús con el fin de que mis clientes naveguen por el sitio web y accedan a los servicios que ofrezco. <h3>1.1.2 - CSS 1 </h3> Como dueño del sistema quiero que la página principal tenga la apariencia previamente planeada con el fin de causar una buena primera impresión en mis clientes que visiten el sitio web.
+        </p>
+        <p className="App">
+        <h3>1.2.1 - HTML 2 </h3>Como dueño del sistema quiero que el sitio web tenga una página de "sign-in" o creación de cuenta de los usuarios con el fin de que esto ayude a identificar a los clientes interesados. <h3>1.2.2 - CSS 2 </h3>Como dueño del sistema quiero que la página de "sign-in" tenga la apariencia previamente planeada con el fin de causar una buena impresión en mis clientes que visiten el sitio web.
+        </p>
+        <p className="App">
+        <h3>1.3.1 - HTML 3 </h3>Como dueño del sistema quiero que el sitio web cuente con una página de "log-in" de los usuarios con el fin de brindarles seguridad a estos últimos.<h3>1.3.2 - CSS 3 </h3>Como dueño del sistema quiero que la página de "log-in" cuente con la apariencia previamente planeada con el fin de causar una buena impresión en mis clientes que visiten el sitio web.
+        </p>
+        <p className="App">
+        <h3>1.4.1 - HTML 4 </h3>Como dueño del sistema quiero que haya una página de soporte y servicio al cliente dentro del sitio web con el fin de que ella dé calidad al servicio ofrecido.<h3>1.4.2 - CSS 4 </h3>Como dueño del sistema quiero que la página de soporte y servicio al cliente tenga la apariencia previamente planeada con el fin de causar una buena impresión en mis clientes que visiten el sitio web.
+        </p>
+        <p className="App">
+        <h3>1.5.1 - HTML 5 </h3>Como dueño del sistema quiero que al sitio web se le diseñe una página la cual permite a mis clientes visualizar el estatus de su vehículo dentro del taller con el fin de que esta característica dote de gran usabilidad al sitio web.<h3>1.5.2 - CSS 5 </h3>Como dueño del sistema quiero que la página que le permita a cada uno de mis clientes vizualizar el estatus de su vehículo en el taller cuente con la apariencia previamente planeada con el fin de causar una buena impresión en mis clientes.
+        </p>
+        <p className="App">
+        <h3>1.6.1 - HTML 6 </h3>Como dueño del sistema quiero que el sitio web contenga una página en donde mis clientes puedan registrar nuevos vehículos con el fin de agilizar las transacciones de mis clientes.<h3>1.6.2 - CSS 6 </h3>Como dueño del sistema quiero que la página de registro de nuevos vehículos tenga la apariencia previamente planeada con el fin de causar una buena impresión en mis clientes.
+        </p>
+        <p className="App">
+        <h3>1.7 - Pruebas </h3>Como dueño del sistema quiero que se lleven a cabo pruebas de usabilidad y experiencia del usuario en todas las páginas del sitio con el fin de garantizar un entorno funcional al sitio.
+        </p>
+        <p className="App">
+        <h3>1.8 - Parche </h3>Como dueño del sistema quiero que se corrijan errores o se agreguen funcionalidades según los resultados de las pruebas con el fin de que se pulan todos los aspectos del proyecto y de que se me entregue el mejor producto posible.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
